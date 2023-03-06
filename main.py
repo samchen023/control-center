a248b2ca6814d16b2d3fa7e59ee193e08fed4de5
samchen <gta45404@gmail.com>
2023-03-06 18:00:22 +0800

    add checkupdate functon

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from tkinter.tix import WINDOW
 from tkinter import *
 import tkinter.font as tkFont
 import webbrowser
+import requests
+
+APP_VERSION = 'v0.0.1'
 
 
 def on_submit():
@@ -42,8 +45,40 @@ def createNewWindow():
 
 
 def createupdateWindow():
+    repo_owner = "samchen023"
+    repo_name = "control-center"
+
+    response = requests.get(
+        f"https://api.github.com/repos/{repo_owner}/{repo_name}/releases?per_page=1")
+    if response.status_code == 200:
+        try:
+            release_data = response.json()[0]
+            latest_release_tag_name = release_data["name"]
+            if latest_release_tag_name <= APP_VERSION:
+                message = f"You are running version {APP_VERSION} of {repo_name}, which is up to date."
+                show_button = False
+            else:
+                message = f"A new version of {repo_name} ({latest_release_tag_name}) is available. You are running version {APP_VERSION}."
+                show_button = True
+        except (KeyError, IndexError):
+            message = "Failed to retrieve release data from GitHub"
+            show_button = False
+    else:
+        message = "Failed to retrieve release data from GitHub"
+        show_button = False
+
     newWindow = tk.Toplevel(root)
-    newWindow.geometry("400x200")
+    newWindow.geometry("600x200")
+    newWindow.title(f"UPDATE")
+    label = tk.Label(newWindow, text=message)
+    label.pack(padx=10, pady=10)
+    if show_button:
+        button = tk.Button(newWindow, text="UPDATE", command=lambda: webbrowser.open(
+            release_data["html_url"]))
+        button.pack(padx=10, pady=10)
+    close_button = tk.Button(newWindow, text="Close",
+                             command=newWindow.destroy)
+    close_button.pack(padx=10, pady=10)
 
 
 root = tk.Tk()
@@ -59,12 +94,13 @@ root.config(menu=menu)
 submenu1 = tk.Menu(activebackground="gray", tearoff=0)
 menu.add_cascade(label="file", menu=submenu1)
 
-submenu1.add_command(label="exit", command=root.destroy)
-submenu1.add_command(label="info", command=createNewWindow)
+
+submenu1.add_command(label="INFO", command=createNewWindow)
+submenu1.add_command(label="EXIT", command=root.destroy)
 
 submenu2 = tk.Menu(tearoff=0)
 menu.add_cascade(label="help", menu=submenu2)
-submenu2.add_command(label="update", command=createupdateWindow)
+submenu2.add_command(label="Update", command=createupdateWindow)
 
 label = tk.Label(root, text="Connected WIFI:")
 label.pack(side="top")
