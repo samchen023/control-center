5a3fad9d6015fd4333feb04b02e8eaf2340380bf
samchen <gta45404@gmail.com>
2023-03-14 20:19:47 +0800

    add spotify curent playing song

--- main.py
+++ main.py
@@ -6,8 +6,22 @@ from tkinter import *
 import tkinter.font as tkFont
 import webbrowser
 import requests
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+import urllib.request
+import io
+from PIL import Image, ImageTk
 
-APP_VERSION = "v1.1.0"
+APP_VERSION = "v1.2.0"
+
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        client_id="",
+        client_secret="",
+        redirect_uri="http://localhost:8888/callback",
+        scope="user-read-playback-state",
+    )
+)
 
 
 def on_submit():
@@ -109,9 +123,56 @@ def hide_refresh_label():
     refresh_label.config(text="")
 
 
+def update_current_track():
+    current_track = sp.current_playback()
+    if current_track is not None and "item" in current_track:
+        track_name = current_track["item"]["name"]
+        track_label.config(text="Currently Playing: " + track_name)
+        image_url = current_track["item"]["album"]["images"][0]["url"]
+        with urllib.request.urlopen(image_url) as url:
+            image_data = url.read()
+        image = Image.open(io.BytesIO(image_data))
+        # Resize the image to 100x100 pixels
+        image = image.resize((100, 100), Image.ANTIALIAS)
+        photo_image = ImageTk.PhotoImage(image)
+        image_label.config(image=photo_image)
+        image_label.image = photo_image  # keep a reference to avoid garbage collection
+    else:
+        track_label.config(text="No music is currently playing.")
+        image_label.config(image=None)
+    root.after(1000, update_current_track)
+
+
+def createspotifyWindow():
+    # Create GUI window
+    spotify = tk.Toplevel()
+    spotify.title("OAuth2 Client Credentials")
+
+    client_id_label = tk.Label(spotify, text="Client ID:")
+    client_id_label.grid(row=0, column=0)
+    client_id_entry = tk.Entry(spotify)
+    client_id_entry.grid(row=0, column=1)
+
+    client_secret_label = tk.Label(spotify, text="Client Secret:")
+    client_secret_label.grid(row=1, column=0)
+    client_secret_entry = tk.Entry(spotify, show="*")
+    client_secret_entry.grid(row=1, column=1)
+
+    submit_button = tk.Button(spotify, text="Submit", command=submit)
+    submit_button.grid(row=2, column=1)
+
+    output_label = tk.Label(spotify)
+    output_label.grid(row=3, column=0, columnspan=2)
+
+    def submit():
+        # Get values from entry fields
+        client_id = client_id_entry.get()
+        client_secret = client_secret_entry.get()
+
+
 root = tk.Tk()
 root.title("program")
-root.geometry("550x350")
+root.geometry("1000x450")
 
 
 menu = tk.Menu(root)
@@ -149,15 +210,38 @@ refresh_label.pack(side="top")
 label = ttk.Label(root, text="Speedtest")
 label.pack(side="top", pady=5)
 
-text = tk.Text(root)
-text.pack(side="left")
+speedtest_frame = Frame(root)
+speedtest_frame.pack(side="left")
+speedtest_frame.config(height=10)
+
+text = tk.Text(speedtest_frame)
+text.grid(row=0, column=0, padx=10, pady=10)
 text.config(width=50, height=5)
 
-
-button = ttk.Button(root, text="Run Speedtest", command=run_speedtest)
-button.pack(side="top", padx=10, pady=5)
+button = ttk.Button(speedtest_frame, text="Run Speedtest", command=run_speedtest)
+button.grid(row=0, column=1, padx=10, pady=10)
 
 refresh_label.config(text="", fg="green")
 
+
+music_frame = Frame(root)
+music_frame.pack(padx=20, pady=20)
+music_frame.config(bg="#182329", width=50)
+
+spotify_label = tk.Label(
+    music_frame, font=("microsoft yahei", 16), fg="#1DB954", text="Spotify"
+)
+spotify_label.pack()
+spotify_label.config(bg="#182329")
+
+track_label = tk.Label(music_frame, font=("microsoft yahei", 16), fg="#ffffff")
+track_label.pack()
+track_label.config(bg="#182329")
+
+image_label = tk.Label(music_frame)
+image_label.pack()
+image_label.config(bg="#182329")
+
 on_submit()
+root.after(1000, update_current_track)
 root.mainloop()
