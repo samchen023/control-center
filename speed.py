57174fdb7a5041d5beddfb4fbc1a6c656da31251
BuildTools <unconfigured@null.spigotmc.org>
2023-04-04 17:04:43 +0800

    change commits

--- speed.py
+++ speed.py
@@ -6,13 +6,10 @@ def get_new_speeds():
     speed_test = st.Speedtest()
     speed_test.get_best_server()
 
-    # Get ping (miliseconds)
     ping = speed_test.results.ping
-    # Perform download and upload speed tests (bits per second)
     download = speed_test.download()
     upload = speed_test.upload()
 
-    # Convert download and upload speeds to megabits per second
     download_mbs = round(download / (10**6), 2)
     upload_mbs = round(upload / (10**6), 2)
 
